2039b47fb5aa9f3f015ebeda26c7ad067c7bb001
daiki431 <dai.take431@gmail.com>
2019-05-31 12:16:07 +0900

    singerの実装

--- Zundoko1/band.cs
+++ Zundoko1/band.cs
@@ -6,28 +6,44 @@ using System.Threading.Tasks;
 
 namespace Zundoko1
 {
-    class band
+    class Band
     {
-        static void Band()
+        static int[] count=new int[100000];
+        public Band()
         {
-            Random r = new Random();
-            singer zundokosinger;
-            zundokosinger = new singer();
-            
+            int i = 0;
+            int f = 0;
+            Singer zundokoSinger = new Singer();
+            while (true)
+            {
+                count[f] = zundokoSinger.Song();
+                if (count[f] == 0)
+                    Console.Write("ズン");
+                else if (count[f] == 1)
+                    Console.WriteLine("ドコ");
+                else
+                    Console.Write("・");
+
+                if (f >= 5 && count[f - 5] == 0 && count[f - 4] == 2 && count[f - 3] == 0 && count[f - 2] == 0 && count[f - 1] == 0 && count[f] == 1)
+                {
+                    if (f >= 6 && count[f - 6] == 1)
+                        break;
+                    if (f == 5)
+                        break;
+                }
+                f++;
+            }
         }
         
     }
 
-    class singer
+    class Singer
     {
-        public void Song(int i)
+        Random r = new Random();
+
+        public int Song()
         {
-            if (i == 0)
-                Console.Write("ズン");
-            else if (i == 1)
-                Console.Write("ドコ");
-            else
-                Console.Write("・");
+            return r.Next(3);
         }
     }
 }
